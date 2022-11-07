895c6d7b0ec5088fc892bc01ae444fcb1d6b45e8
Vit <privit@rambler.ru>
2022-11-07 16:49:04 +0300

    Задача3

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,19 @@ public class Main
     public static void main(String[] args)
     {
       // Задание 1
-       var dog = 8;
+       var dog = 8.0;
        var cat = 3.6;
        var paper = 763789;
-      // Задание 2
+
+       // Задание 2
        dog = dog+4;
        cat = cat+4;
        paper = paper+4;
+
+       // Задание 3
+       dog = dog-3.5;
+       cat = cat-1.6;
+       paper = paper -7639;
 
         System.out.println(dog);
         System.out.println(cat);
