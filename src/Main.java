4fda200bd860fa5aca840884b50a947ad204c227
Vit <privit@rambler.ru>
2022-11-07 15:13:11 +0300

    Залача2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,15 @@ public class Main
 {
     public static void main(String[] args)
     {
-       var dog=8;
-       var cat=3.6;
-       var paper=763789;
+      // Задание 1
+       var dog = 8;
+       var cat = 3.6;
+       var paper = 763789;
+      // Задание 2
+       dog = dog+4;
+       cat = cat+4;
+       paper = paper+4;
+
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
